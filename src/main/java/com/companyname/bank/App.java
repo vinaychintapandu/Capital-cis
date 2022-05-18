1eeb6657eaeb401b71a18dd995b66bd0a177450e
vinaychintapandu <91235856+vinaychintapandu@users.noreply.github.com>
2022-05-18 20:07:24 +0530

    modified for trigger git scm polling

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World modified for poll scm" );
+        System.out.println( "Hello World modified for git scm polling" );
     }
 }
