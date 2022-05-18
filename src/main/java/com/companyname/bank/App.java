ea2ad309539d8a31656cbca13c5bb0b2736275de
vinaychintapandu <91235856+vinaychintapandu@users.noreply.github.com>
2022-05-18 20:11:03 +0530

    githubhook

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World modified for git scm polling" );
+        System.out.println( "Hello World modified for git scmpolling" );
     }
 }
